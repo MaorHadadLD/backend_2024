00cc854512195a5a00e375f784cbc5ff05f4f9ee
Maor Hadad <maorhadad94@gmail.com>
2024-01-28 23:26:28 +0200

    add student route in app.js

--- App.js
+++ App.js
@@ -3,13 +3,7 @@ const app = express();
 const dotenv = require("dotenv").config();
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.get("/home", (req,res) => {
-    res.send("Hello Home!");
-});
+const studentRoute = require("./routes/student_route");
 
 app.listen(process.env.PORT, () => {
     console.log('Example app listening at http://localhost:${process.env.PORT}');
