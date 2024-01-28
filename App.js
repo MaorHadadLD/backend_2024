6a1283e08ae009dd1a4f0104b0c165e080f06c22
Maor Hadad <maorhadad94@gmail.com>
2024-01-28 23:27:14 +0200

    add use func

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv").config();
 
 const studentRoute = require("./routes/student_route");
 
+app.use(studentRoute);
+
 app.listen(process.env.PORT, () => {
     console.log('Example app listening at http://localhost:${process.env.PORT}');
 });
