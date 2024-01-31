b96ab29ab9f57073522c1c5b549f6a313e33301a
Maor Hadad <maorhadad94@gmail.com>
2024-01-31 16:59:25 +0200

    mongoos + body-parser

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express();
 const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.BASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () =>  console.log("connected to db"));
 
 
 const studentRoute = require("./routes/student_route");
@@ -8,6 +14,6 @@ const studentRoute = require("./routes/student_route");
 app.use(studentRoute);
 
 app.listen(process.env.PORT, () => {
-    console.log('Example app listening at http://localhost:${process.env.PORT}');
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
 
