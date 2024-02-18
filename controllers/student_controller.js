08f3509ee4131b22fd4b4d10491761fe37d2b5f6
Maor Hadad <maorhadad94@gmail.com>
2024-02-18 18:41:55 +0200

    add let student to getStudents

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -4,13 +4,13 @@ const Student = require('../models/student_model');
 const getStudents = async (req, res) => {
     console.log('getStudent');
     try {
+        let students;
         if (req.query.name) {
-            const students = await Student.find({name: req.query.name});
-            res.status(200).send(students);
+           students = await Student.find({name: req.query.name});
         } else {
-        const students = await Student.find();
-        res.status(200).send(students);
+            students = await Student.find();
         }
+        res.status(200).send(students);
     } catch (error) {
         console.log(error);
         res.status(400).send(error.message);
