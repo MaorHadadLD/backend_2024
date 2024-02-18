4843bf0c0e0d0def563d359141c5b6d68745ce66
Maor Hadad <maorhadad94@gmail.com>
2024-02-18 16:23:32 +0200

    install body-parser

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ const express = require("express")
 const app = express();
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 mongoose.connect(process.env.BASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () =>  console.log("connected to db"));
 
-
-const studentRoute = require("./routes/student_route");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/students", studentRoute);
 app.use("/posts", postRoute);
