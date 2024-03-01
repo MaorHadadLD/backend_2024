0ef40e4630e633d49f751641d7bb5ad97849ac3e
Maor Hadad <maorhadad94@gmail.com>
2024-03-02 01:28:26 +0200

    add post test

--- tests/student.test.js
+++ tests/student.test.js
@@ -17,11 +17,44 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-describe("Student tests", () => {
-    test("Test Student get all", async () => {
+
+const students = [
+    {
+        name: 'John Doe',
+        _id : '12345',
+        age : 22
+    },
+    {
+        name: 'Jane Doe',
+        _id: '12346',
+        age: 23
+    }
+];
+
+describe("Student ", () => {
+    test("Get /student - empty collection", async () => {
         const res = await request(app).get('/student');
         expect(res.statusCode).toBe(200);
         const data = res.body;
         expect(data).toEqual([]);
     });
-});  
+
+    test("POST /student", async () => {
+        const res = await request(app).post("/student")
+          .send(students[0]).set('Authorization', 'Bearer ' + testUser.accessToken);
+        expect(res.statusCode).toEqual(201);
+        expect(res.body.name).toEqual(students[0].name);
+        // studentId = res.body._id; //
+        const res2 = await request(app).get("/student")
+          .set('Authorization', 'Bearer ' + testUser.accessToken);
+        expect(res2.statusCode).toBe(200);
+        const data = res2.body;
+        expect(data[0].name).toBe(students[0].name);
+        expect(data[0]._id).toBe(students[0]._id);
+        expect(data[0].age).toBe(students[0].age);
+      });
+
+
+    
+});
+
