37c4cf90030850b244bb6134e1d86e1335d262a9
Maor Hadad <maorhadad94@gmail.com>
2024-03-02 01:03:59 +0200

    initApp with Promise

--- App.js
+++ App.js
@@ -6,18 +6,21 @@ const studentRoute = require("./routes/studentRoute");
 const postRoute = require("./routes/postRoute");
 const bodyParser = require("body-parser");
 
-const initApp = (callback) => {
-    const db = mongoose.connection;
-    db.on("error", (error) => console.error(error));
-    db.once("open", () =>  console.log("connected to db"));
-    mongoose.connect(process.env.BASE_URL).then(() => {
+const initApp = () => {
+    const promise = new Promise(async (resolve, reject) => {
+        const db = mongoose.connection;
+        db.on("error", (error) => console.error(error));
+        db.once("open", () =>  console.log("connected to db"));
+        await mongoose.connect(process.env.BASE_URL)
         app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use("/student", studentRoute);
-    app.use("/post", postRoute);
-    callback(app);
+        app.use("/student", studentRoute);
+        app.use("/post", postRoute);
+        resolve();
     });
+    return promise;
+    
 };
 
 
