836c8a7481e931a739bdac02a89363ac66d6f376
Maor Hadad <maorhadad94@gmail.com>
2024-03-02 16:01:14 +0200

    student controller

--- src/controllers/student_controller.ts
+++ src/controllers/student_controller.ts
@@ -9,27 +9,12 @@ const getStudents = async (req: Request, res: Response) => {
 
 const getStudentById = async (req: Request, res: Response) => {
     console.log(req.params.id);
-    try{
-        const student = await Student.findById(req.params.id);
-        if (student) {
-            res.status(200).send(student);
-        } else {
-            res.status(404).send("Student not found");
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error.message);
-    }
+    baseController.getById(Student, req, res);
 };
 
 const postStudents = async (req: Request, res: Response) => {
     console.log("student post");
-    try{
-        const student = await Student.create(req.body);
-        res.status(201).send(student);
-    } catch (error) {
-        res.status(400).send(error.massage);
-    }
+    baseController.post(Student, req, res);
 };
 
 
@@ -39,14 +24,8 @@ const putStudents = (req: Request, res: Response) => {
 };
 
 const deleteStudents = async (req, res) => {
-    res.send("student delete");
-    try {
-        await Student.findByIdAndDelete(req.params.id);     
-        return res.status(200).send();  
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error.message);
-    };
+    console.log("student delete");
+    baseController.remove(Student, req, res);
 };
 
 export default {
