5e5f19fe7ef80d7c725a35a4048ba60c99666b04
Maor Hadad <maorhadad94@gmail.com>
2024-03-02 16:23:20 +0200

    update base controller

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -1,67 +1,69 @@
-import { Request, Response } from 'express'
+import { Request, Response } from "express";
+import mongoose from "mongoose";
 
-class BaseController {
-    ItemModel: any;
-    constructor(ItemModel: any) {
-        this.ItemModel = ItemModel;
+class BaseController<ModelType> {
+    itemModel: mongoose.Model<ModelType>;
+    constructor(itemModel: mongoose.Model<ModelType>) {
+        this.itemModel = itemModel;
     }
-    async get =  (req: Request, res: Response) {
-    console.log('get');
-    try {
-        if (req.query.name) {
-           const item = await ItemModel.find({name: req.query.name});
-           res.status(200).send(item);
-        } else {
-            const item = await ItemModel.find();
-            res.status(200).send(item);
+    async get(req: Request, res: Response) {
+        console.log("get");
+        try {
+            if (req.query.name) {
+                const item = await this.itemModel.find({ name: req.query.name });
+                res.status(200).send(item);
+            } else {
+                const item = await this.itemModel.find();
+                res.status(200).send(item);
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error.message);
         }
-        
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error.message);
-    } 
     }
-const getById = async (ItemModel,req: Request, res: Response) => {
-    console.log(req.params.id);
-    try{
-        const item = await ItemModel.findById(req.params.id);
-        if (item) {
-            res.status(200).send(item);
-        } else {
-            res.status(404).send("not found");
+
+    async getById(req: Request, res: Response) {
+        console.log(req.params);
+        try {
+            const item = await this.itemModel.findById(req.params.id);
+            if (!item) {
+                return res.status(404).send("not found");
+            } else {
+                return res.status(200).send(item);
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error.message);
         }
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error.message);
     }
-};
 
-const post = async (ItemModel,req: Request, res: Response) => {
-    console.log("post");
-    try{
-        const item = await ItemModel.create(req.body);
-        res.status(201).send(item);
-    } catch (error) {
-        res.status(400).send(error.massage);
+    async post(req: Request, res: Response) {
+        console.log("student post ");
+        try {
+            const student = await this.itemModel.create(req.body);
+            res.status(201).send(student);
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error.message);
+        }
     }
-};
 
+    //updatye a sudent with the given id
+    put(req: Request, res: Response) {
+        console.log("student put");
+        res.status(400).send("Not implemented");
+    }
 
-const put = (ItemModel,req: Request, res: Response) => {
-    console.log("student put");
-    res.status(400).send("Not implemented");
-};
-
-const remove = async (ItemModel,req, res) => {
-    res.send("student delete");
-    try {
-        await ItemModel.findByIdAndDelete(req.params.id);     
-        return res.status(200).send();  
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error.message);
-    };
-};
+    async remove(req: Request, res: Response) {
+        console.log("student delete");
+        try {
+            await this.itemModel.findByIdAndDelete(req.params.id);
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error.message);
+        }
+    }
 }
 
 export default BaseController;
