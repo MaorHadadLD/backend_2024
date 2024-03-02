ac7ecc233737faac76e729932e13ff4e49df52c7
Maor Hadad <maorhadad94@gmail.com>
2024-03-02 16:16:39 +0200

    update student model

--- src/models/studentModel.ts
+++ src/models/studentModel.ts
@@ -1,24 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
 
 export interface IStudent {
-    name: string;
-    _id: string;
-    age: number;
+  name: string;
+  _id: string;
+  age: number;
 }
-const studentSchema = new mongoose.Schema ({
-    name: {
-        type : String,
-        required: true
-    },
-    _id: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
 
+const studentSchema = new mongoose.Schema<IStudent>({
+  name: {
+    type: String,
+    required: true,
+  },
+  _id: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
 });
 
-export default mongoose.model('Student', studentSchema);
+export default mongoose.model<IStudent>("Student", studentSchema);
