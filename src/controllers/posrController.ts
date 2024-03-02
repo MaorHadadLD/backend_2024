9bde3fc7fd91e47e86e006e3c680cace606bc7ac
Maor Hadad <maorhadad94@gmail.com>
2024-03-02 15:51:19 +0200

    post controller

--- src/controllers/posrController.ts
+++ src/controllers/posrController.ts
@@ -1,15 +1,9 @@
 import Post from '../models/postModel';
 import { Request, Response } from 'express'
-
+import baseController from '../controllers/baseController';
 const getPost = async (req: Request, res: Response) => {
     console.log('getStudent');
-    try {
-        const post = await Post.find();
-        res.status(200).send(post);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error.message);
-    } 
+    baseController.get(Post, req, res);
 };
 
 export default { getPost };
