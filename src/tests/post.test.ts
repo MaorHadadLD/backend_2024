d9bb8f24ddf558dd65a987908a2fea73f8b3da76
Maor Hadad <maorhadad94@gmail.com>
2024-03-02 15:26:38 +0200

    post test

---
+++ src/tests/post.test.ts
@@ -0,0 +1,42 @@
+import request from 'supertest';
+import appInit from '../App';
+import mongoose from 'mongoose';
+import Post from '../models/posttModel';
+import { Express } from 'express';
+
+let app: Express;
+beforeAll(async () => {
+    app = await appInit();
+    console.log("beforAll");
+    await Post.deleteMany();
+});
+
+
+afterAll(async () => {
+    console.log('After all tests')
+    await mongoose.connection.close();
+});
+
+
+// const students = [
+//     {
+//         name: 'John Doe',
+//         _id : '12345',
+//         age : 22
+//     },
+//     {
+//         name: 'Jane Doe',
+//         _id: '12346',
+//         age: 23
+//     }
+// ];
+
+describe("Student ", () => {
+    test("Get /post - empty collection", async () => {
+        const res = await request(app).get('/post');
+        expect(res.statusCode).toBe(200);
+        const data = res.body;
+        expect(data).toEqual([]);
+    });
+});
+
