46d5505b9bbb54a368074a94df0a13afc7832cb5
Maor Hadad <maorhadad94@gmail.com>
2024-03-16 00:49:46 +0200

    -authController- register + login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,4 +2,13 @@ import { Request, Response } from "express";
 
 const register = (req: Request, res: Response) => {
     res.send("register");
-    };
+};
+
+const login = (req: Request, res: Response) => {
+    res.send("login");
+};
+
+export default {
+    register,
+    login
+};
