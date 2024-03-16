d4434f441e3edb2cfbf80e5cce567b75a67e86eb
Maor Hadad <maorhadad94@gmail.com>
2024-03-16 03:46:33 +0200

    - studentRoute - add authMiddleware

--- src/routes/studentRoute.ts
+++ src/routes/studentRoute.ts
@@ -1,14 +1,15 @@
 import express from "express";
 const router = express.Router();
 import StudentController from "../controllers/student_controller";
+import authMiddleware from "../controllers/common/authMiddleware";
 
-router.get("/",  StudentController.get.bind(StudentController));
+router.get("/", authMiddleware,  StudentController.get.bind(StudentController));
 router.get("/:id", StudentController.getById.bind(StudentController));
 
-router.post("/",  StudentController.post.bind(StudentController));
+router.post("/", authMiddleware, StudentController.post.bind(StudentController));
 
-router.put("/:id",  StudentController.put.bind(StudentController));
+router.put("/:id", authMiddleware, StudentController.put.bind(StudentController));
 
-router.delete("/:id",  StudentController.remove.bind(StudentController));
+router.delete("/:id", authMiddleware, StudentController.remove.bind(StudentController));
 
 export default router;
