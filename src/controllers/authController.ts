7d9c4ac150b0f421c62b8dea59eb9635af02ded6
Maor Hadad <maorhadad94@gmail.com>
2024-03-16 02:44:19 +0200

    - auteController - register

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express";
+import User from "../models/userModel";
 
 const register = (req: Request, res: Response) => {
-    res.status(400).send("register");
+    console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (email == null || password == null) {
+        return res.status(400).send("missing email or password");
+    }
+
+    try {
+        const user = await User.findOne({ email: email });
+        if (user) {
+            res.status(400).send("user already exists");
+        }
+        
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error.message);
+    }
 };
 
 const login = (req: Request, res: Response) => {
