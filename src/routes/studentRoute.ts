80cb6c63a864b291086057b59bc43c53e7caf559
Maor Hadad <maorhadad94@gmail.com>
2024-03-25 21:25:59 +0200

    update student route

--- src/routes/studentRoute.ts
+++ src/routes/studentRoute.ts
@@ -4,6 +4,7 @@ import StudentController from "../controllers/student_controller";
 import authMiddleware from "../controllers/common/authMiddleware";
 
 router.get("/", authMiddleware,  StudentController.get.bind(StudentController));
+
 router.get("/:id", StudentController.getById.bind(StudentController));
 
 router.post("/", authMiddleware, StudentController.post.bind(StudentController));
